c7b77b8b982a874e34bceb9b43cbb850e3bb4cd7
Bhargavi Kulkarni <bhargavikulkarni1595@gmail.com>
2026-07-23 03:09:47 +0530

    Add project routes: create, list, view, add members

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/projects', require('./routes/projectRoutes'));
 
 app.get('/', (req, res) => {
   res.send('TeamTrack API is running');
